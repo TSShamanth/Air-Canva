08869c9deec163e8db1db52fdc205be0ab58bb79
TSShamanth <157595891+TSShamanth@users.noreply.github.com>
2024-07-16 14:25:13 +0530

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('start').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        files: ['content.js']
-      });
-    });
-  });
-  
